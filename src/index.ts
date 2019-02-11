13022933c3c2dc8b4dc0ce4c0b735e3221733fba
“ALEX <alexgtr84@gmail.com>
2019-02-11 15:03:43 +0100

    Add changes to alice

--- src/index.ts
+++ src/index.ts
@@ -3,21 +3,6 @@ import * as moment from 'moment'
 
 const app = express()
 
-app
-    .get('/users/:id([0-9]+)', (req, res) => {
-        const userId: string = req.params.id
-        if (userId === '123') {
-            res.send({
-                name: 'Jan Klaassen',
-                age: 60
-            })
-        }
-        else {
-            res.status(404).send(`User ${userId} not found!`)
-        }
-    })
-    .listen(12345, () => console.log('Listening on port 12345'))
-
 interface UserEntity {
     id: number
     firstName: string
@@ -32,7 +17,46 @@ interface UserResponse {
     }
 }
 
-const toUser = (entity: UserEntity): UserResponse => {
+const alice: UserEntity = {
+    id: 1,
+    firstName: 'Alice',
+    lastName: 'Pleasance Liddell',
+    birthDate: new Date(1865, 5, 4)
+  }
+  
+  const getUser = (id: number) => new Promise<UserEntity | null>((resolve, reject) => {
+    setTimeout(() => {
+      if (id === 123) resolve(alice)
+      else if (id > 1000) reject(new Error('getUser error!'))
+      else resolve(null)
+    }, 500)
+  })
+
+
+app
+    // .get('/users/:id([0-9]+)', (req, res) => {
+    //     const userId: string = req.params.id
+    //     if (userId === '123') {
+    //         res.send({
+    //             name: 'Jan Klaassen',
+    //             age: 60
+    //         })
+    //     }
+    //     else {
+    //         res.status(404).send(`User ${userId} not found!`)
+    //     }
+    // })
+    .get('/users/:id([0-9]+)', async (req, res) => {
+        const response = undefined
+
+        
+      
+        if (response) res.send(toUser(response))
+        else res.status(404).send(`User not found!`)
+      })
+    .listen(12345, () => console.log('Listening on port 12345'))
+
+  const toUser = (entity: UserEntity): UserResponse => {
     const currentDate = moment()
     const ageYears = moment.duration(currentDate.diff(entity.birthDate));
     const userAge = Math.floor(ageYears.asYears());
@@ -45,62 +69,59 @@ const toUser = (entity: UserEntity): UserResponse => {
     }
 }
 
-const userOne: UserEntity = {
-    id: 1,
-    firstName: 'User',
-    lastName: 'One',
-    birthDate: new Date('01-01-2000')
-}
+// const userOne: UserEntity = {
+//     id: 1,
+//     firstName: 'User',
+//     lastName: 'One',
+//     birthDate: new Date('01-01-2000')
+// }
 
+// console.log(toUser(userOne))
 
+// interface YourInterface {
+//     nickname: string
+//     color: "blue" | "white"
+//     age: number
+//     websites: string[]
+//     job:
+//     {
+//         description: string
+//         salary: number
+//         info?: string
+//     } 
+//     // |
+//     // {
+//     //     description: string
+//     //     salary: number
+//     // }
+// }
 
-console.log(toUser(userOne))
+// const cheshireCat: YourInterface = {
+//     nickname: 'cat',
+//     color: 'blue',
+//     age: 15,
+//     websites: [
+//         'http://www.catsareawesome.com',
+//         'http://www.dogsarelame.biz'
+//     ],
+//     job: {
+//         description: 'Scare people',
+//         salary: 500,
+//         info: 'only works on sundays'
+//     }
+// }
 
-
-interface YourInterface {
-    nickname: string
-    color: "blue" | "white"
-    age: number
-    websites: string[]
-    job:
-    {
-        description: string
-        salary: number
-        info?: string
-    } 
-    // |
-    // {
-    //     description: string
-    //     salary: number
-    // }
-}
-
-const cheshireCat: YourInterface = {
-    nickname: 'cat',
-    color: 'blue',
-    age: 15,
-    websites: [
-        'http://www.catsareawesome.com',
-        'http://www.dogsarelame.biz'
-    ],
-    job: {
-        description: 'Scare people',
-        salary: 500,
-        info: 'only works on sundays'
-    }
-}
-
-const whiteRabbit: YourInterface = {
-    nickname: 'rabbie',
-    color: 'white',
-    age: 7,
-    websites: [
-        'http://www.rabbitsrule.com',
-        'http://www.carrots.net',
-        'http://www.rabbitnews.ru'
-    ],
-    job: {
-        description: 'Being fluffy',
-        salary: 1500
-    }
-}
+// const whiteRabbit: YourInterface = {
+//     nickname: 'rabbie',
+//     color: 'white',
+//     age: 7,
+//     websites: [
+//         'http://www.rabbitsrule.com',
+//         'http://www.carrots.net',
+//         'http://www.rabbitnews.ru'
+//     ],
+//     job: {
+//         description: 'Being fluffy',
+//         salary: 1500
+//     }
+// }
